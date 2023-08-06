3d0ddf1577a01816e8872d9271ffa9bd3e75da0f
Deren Teo <s4528554@student.uq.edu.au>
2023-08-06 22:21:47 +1000

    A1Q4: part (a) completed

---
+++ a1/src/double_transform.py
@@ -0,0 +1,54 @@
+"""
+Script associated with Q4.
+
+Implementation of the double transform algorithm to Fourier transform two real
+N-point sequences using one commplex N-point transform.
+"""
+
+import numpy as np
+
+from scipy.fft import fft
+
+### DSP FUNCTIONS ##############################################################
+
+def ev(H: np.array) -> np.array:
+    """
+    Returns the even component of the given sequence `H`.
+    """
+    H_minus = np.concatenate([H[:1], H[-1:0:-1]])
+    return 0.5 * (H + H_minus)
+
+def od(H: np.array) -> np.array:
+    """
+    Returns the odd component of the given sequence `H`.
+    """
+    H_minus = np.concatenate([H[:1], H[-1:0:-1]])
+    return 0.5 * (H - H_minus)
+
+### ENTRYPOINT #################################################################
+
+def main():
+
+    ### PART A ###
+
+    x = np.array([1, 2, 4, 4, 5, 3, 7, 8])
+    y = np.array([1, 5, 3, 1, 3, 5, 3, 7])
+    print(f"{fft(x) = }")
+    print(f"{fft(y) = }")
+
+    Z = fft(np.array([a+b*1j for a, b in zip(x, y)]))
+    print(f"{Z = }")
+
+    print(f"{ev(np.real(Z)) = }")
+    print(f"{od(np.imag(Z)) = }")
+    print(f"{od(np.real(Z)) = }")
+    print(f"{ev(np.imag(Z)) = }")
+
+    X = (ev(np.real(Z)) + 1j * od(np.imag(Z)))
+    Y = (od(np.real(Z)) + 1j * ev(np.imag(Z))) * -1j
+    print(f"{X = }")
+    print(f"{Y = }")
+
+
+if __name__ == "__main__":
+    main()
